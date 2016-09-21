6f1f6a50ce0f535954de5f67a31e9509209a40f5
gejiawen <806717031@qq.com>
2016-09-21 19:25:00 +0800

    ok

---
+++ rc.js
@@ -0,0 +1,18 @@
+/**
+ * @file: rc.js
+ * @author: gejiawen
+ * @date: 9/21/16 17:01
+ * @description: rc.js
+ *
+ * @loglevel
+ * default: warn
+ * values: silent, error, warn, http, info, verbose, silly
+ */
+
+
+
+module.exports = {
+    registry: 'https://registry.npm.taobao.org/',
+    disturl: 'http://npm.taobao.org/mirrors/node/',
+    loglevel: 'http'
+}
